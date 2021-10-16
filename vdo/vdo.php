e853be56a13da521496b7f687d048642d85cf980
A <73538598+eycn@users.noreply.github.com>
2021-10-17 03:45:34 +0800

    Delete vdo.php

--- vdo/vdo.php
+++
@@ -1,9 +0,0 @@
-<?php
-/**
-* PHP随机图显示
-*/
-header('Content-Type: text/html; charset=UTF-8');
-$vdo_array = glob("vdo/*.mp4",GLOB_BRACE);
-$vdo = array_rand($vdo_array);
-header("location:$vdo_array[$vdo]");
-?>
